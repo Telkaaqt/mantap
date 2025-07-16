bfab7b8fac57f0b33d1b3a2aef84c2e91ec02a7c
levine23 <annacantik461@gmail.com>
2025-07-17 07:58:49 +0900

    Update config.py

--- config/config.py
+++ config/config.py
@@ -21,7 +21,7 @@ BOT_USERNAME = getenv("BOT_USERNAME" , "AssitantlevineBot1")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 
 CLEANMODE_DELETE_MINS = int(
-    getenv("CLEANMODE_MINS", "1")
+    getenv("CLEANMODE_MINS", "")
 )  # Remember to give value in Seconds
 
 # Custom max audio(music) duration for voice chat. set DURATION_LIMIT in variables with your own time(mins), Default to 60 mins.
