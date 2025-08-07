bf3985182f18cccc50ac2ba9d81cda6c354128be
levine23 <annacantik461@gmail.com>
2025-08-07 09:43:06 +0900

    Update clean.py

--- ChampuMusic/plugins/tools/clean.py
+++ ChampuMusic/plugins/tools/clean.py
@@ -9,11 +9,11 @@ from ChampuMusic.misc import SUDOERS
 
 @app.on_message(filters.command("clean") & SUDOERS)
 async def clean(_, message):
-    A = await message.reply_text("ᴄʟᴇᴀɴɪɴɢ ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs...")
+    A = await message.reply_text("<blockquote>ᴄʟᴇᴀɴɪɴɢ ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs...</blockquote>")
     dir = "downloads"
     dir1 = "cache"
     shutil.rmtree(dir)
     shutil.rmtree(dir1)
     os.mkdir(dir)
     os.mkdir(dir1)
-    await A.edit("ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs ᴀʀᴇ ᴄʟᴇᴀɴᴇᴅ")
+    await A.edit("<blockquote>ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs ᴀʀᴇ ᴄʟᴇᴀɴᴇᴅ</blockquote>")
