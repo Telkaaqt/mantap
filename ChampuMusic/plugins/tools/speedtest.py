bccda6376d4af1bfdd927c25244ac215169e7afc
levine23 <annacantik461@gmail.com>
2025-08-07 09:36:13 +0900

    Update speedtest.py

--- ChampuMusic/plugins/tools/speedtest.py
+++ ChampuMusic/plugins/tools/speedtest.py
@@ -15,13 +15,13 @@ def testspeed(m):
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
-        m = m.edit("⇆ ʀᴜɴɴɪɴɢ ᴅᴏᴡɴʟᴏᴀᴅ sᴩᴇᴇᴅᴛᴇsᴛ...")
+        m = m.edit("<blockquote>⇆ ʀᴜɴɴɪɴɢ ᴅᴏᴡɴʟᴏᴀᴅ sᴩᴇᴇᴅᴛᴇsᴛ...</blockquote>")
         test.download()
-        m = m.edit("⇆ ʀᴜɴɴɪɴɢ ᴜᴘʟᴏᴀᴅ sᴘᴇᴇᴅᴛᴇsᴛ...")
+        m = m.edit("<blockquote>⇆ ʀᴜɴɴɪɴɢ ᴜᴘʟᴏᴀᴅ sᴘᴇᴇᴅᴛᴇsᴛ...</blockquote>")
         test.upload()
         test.results.share()
         result = test.results.dict()
-        m = m.edit("↻ sʜᴀʀɪɴɢ sᴘᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛ")
+        m = m.edit("<blockquote>↻ sʜᴀʀɪɴɢ sᴘᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛ</blockquote>")
     except Exception as e:
         return m.edit(e)
     return result
@@ -29,10 +29,10 @@ def testspeed(m):
 
 @app.on_message(filters.command(SPEEDTEST_COMMAND) & SUDOERS)
 async def speedtest_function(client, message):
-    m = await message.reply_text("ʀᴜɴɴɪɴɢ sᴘᴇᴇᴅᴛᴇsᴛ")
+    m = await message.reply_text("<blockquote>ʀᴜɴɴɪɴɢ sᴘᴇᴇᴅᴛᴇsᴛ</blockquote>")
     loop = asyncio.get_event_loop_policy().get_event_loop()
     result = await loop.run_in_executor(None, testspeed, m)
-    output = f"""**sᴘᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛ**
+    output = f"""<blockquote expandable>**sᴘᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛ**
     
 <u>**ᴄʟɪᴇɴᴛ:**</u>
 **ɪsᴘ :** {result['client']['isp']}
@@ -43,7 +43,7 @@ async def speedtest_function(client, message):
 **ᴄᴏᴜɴᴛʀʏ :** {result['server']['country']}, {result['server']['cc']}
 **sᴘᴏɴsᴏʀ :** {result['server']['sponsor']}
 **ʟᴀᴛᴇɴᴄʏ :** {result['server']['latency']}  
-**ᴘɪɴɢ :** {result['ping']}"""
+**ᴘɪɴɢ :** {result['ping']}</blockquote>"""
     msg = await app.send_photo(
         chat_id=message.chat.id, photo=result["share"], caption=output
     )
