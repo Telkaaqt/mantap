53cbf42f94136f0497f3ac5a5766ccbde15fe819
Levine <annacantik461@gmail.com>
2025-08-11 09:25:49 +0900

    Update __main__.py

--- ChampuMusic/__main__.py
+++ ChampuMusic/__main__.py
@@ -20,16 +20,16 @@ async def init():
         and not config.STRING5
     ):
         LOGGER("ChampuMusic").error(
-            "Assistant client variables not defined, exiting..."
+            "ᴀssɪsᴛᴀɴᴛ ᴄʟɪᴇɴᴛ ᴠᴀʀɪᴀʙʟᴇs ɴᴏᴛ ᴅᴇғɪɴᴇᴅ, ᴇxɪᴛɪɴɢ..."
         )
         return
-
     if not config.SPOTIFY_CLIENT_ID and not config.SPOTIFY_CLIENT_SECRET:
         LOGGER("ChampuMusic").warning(
-            "No Spotify vars defined. Your bot won't be able to play Spotify queries..."
+            "ɴᴏ sᴘᴏᴛɪғʏ ᴠᴀʀs ᴅᴇғɪɴᴇᴅ. ʏᴏᴜʀ ʙᴏᴛ ᴡᴏɴ'ᴛ ʙᴇ ᴀʙʟᴇ ᴛᴏ ᴘʟᴀʏ sᴘᴏᴛɪғʏ ǫᴜᴇʀɪᴇs..."
         )
 
-    # Load banned users
+    await app.start()
+    await userbot.start()
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -40,17 +40,20 @@ async def init():
     except Exception:
         pass
 
-    # Load plugins
     for all_module in ALL_MODULES:
         imported_module = importlib.import_module(all_module)
+
         if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
             if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
                 HELPABLE[imported_module.__MODULE__.lower()] = imported_module
-    LOGGER("ChampuMusic.plugins").info("Successfully imported modules...")
+    LOGGER("ChampuMusic.plugins").info("sᴜᴄᴄᴇssғᴜʟʟʏ ɪᴍᴘᴏʀᴛᴇᴅ ᴍᴏᴅᴜʟᴇs...")
 
-    # Start main bot voice handler
     await Champu.start()
     await Champu.decorators()
-    LOGGER("ChampuMusic").info("Successfully started")
-
+    LOGGER("ChampuMusic").info("\x43\x68\x61\x6D\x70\x75\x20\x42\x6F\x74\x20\x68\x61\x73\x20\x62\x65\x65\x6E\x20\x73\x75\x63\x63\x65\x73\x73\x66\x75\x6C\x6C\x79\x20\x73\x74\x61\x72\x74\x65\x64\x2E\x0A\x0A\x40\x54\x68\x65\x43\x68\x61\x6D\x70\x75\x20")
     await idle()
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop_policy().get_event_loop().run_until_complete(init())
+    LOGGER("ChampuMusic").info("sᴛᴏᴘᴘɪɴɢ ᴄʜᴀᴍᴘᴜᴍᴜsɪᴄ! ɢᴏᴏᴅʙʏᴇ")
